a197915723a6082adafae1bbb4b9ab628288b7ef
Suhas Dissanayake <suhasdissa@gmail.com>
2023-11-09 19:15:14 +0530

    Use jar library instead of java files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
     implementation("androidx.navigation:navigation-compose:2.7.5")
-    implementation(project(mapOf("path" to ":arity")))
+    implementation(files("arity-2.1.2.jar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
