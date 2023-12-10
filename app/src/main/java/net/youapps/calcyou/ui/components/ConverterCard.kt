d0672e1a9a90370c06ed0853ba84eb56ce764e1a
Suhas Dissanayake <suhasdissa@gmail.com>
2023-12-10 17:50:01 +0530

    style: fix converter icon color

--- app/src/main/java/net/youapps/calcyou/ui/components/ConverterCard.kt
+++ app/src/main/java/net/youapps/calcyou/ui/components/ConverterCard.kt
@@ -12,11 +12,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -44,7 +46,8 @@ fun ConverterCard(icon: ImageVector, @StringRes title: Int, onClick: () -> Unit)
             Image(
                 imageVector = icon,
                 contentDescription = null,
-                modifier = Modifier.size(36.dp)
+                modifier = Modifier.size(36.dp),
+                colorFilter = ColorFilter.tint(LocalContentColor.current)
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
