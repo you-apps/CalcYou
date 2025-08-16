bf6eedc9ec6839ff9ba7cc9e9c43c3f1cd189ca8
Bnyro <bnyro@tutanota.com>
2025-08-16 23:07:20 +0200

    chore: bump version to v3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "net.youapps.calcyou"
         minSdk = 21
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.0"
+        versionCode = 5
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
