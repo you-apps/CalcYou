11787fcc06a600eb057f6b62aa9cd158d1d1847d
Bnyro <bnyro@tutanota.com>
2026-03-30 18:09:25 +0200

    chore: bump version to v5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "net.youapps.calcyou"
         minSdk = 23
         targetSdk = 35
-        versionCode = 8
-        versionName = "5.0"
+        versionCode = 9
+        versionName = "5.1"
         // TODO: update 'currentApiVersion' below before each release
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
